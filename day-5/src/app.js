04358c91a3da6e62e2dfb24930d43713994c3c7b
Avnish-kaushik <kaushikavi7002@gmail.com>
2026-08-06 23:33:20 +0530

    Status Codes in API

---
+++ day-5/src/app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+const notes = []
+
+/*POST/notes */
+app.post('/notes', (req, res) => {
+  notes.push(req.body)
+  res.status(201).json({
+    message: 'Note created successfully',
+  })
+})
+
+/* GET/notes */
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    notes: notes,
+  })
+})
+
+/* DELETE/notes/:index */
+app.delete('/notes/:index', (req, res) => {
+  delete notes[req.params.index]
+  res.status(204).json({
+    msg: 'Note deleted successfully',
+  })
+})
+
+/* PATCH /notes/:index */
+app.patch('/notes/:index', (req, res) => {
+  notes[req.params.index].desc = req.body.desc
+  res.status(200).json({
+    msg: 'Note updated successfully',
+  })
+})
+
+module.exports = app
